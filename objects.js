74d260272ab46374ce9661962d0c3d53f978465c
pirushanp <pirushanp@gmail.com>
2017-10-27 09:13:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName ){
-  var playlist = {Kanye:"Gold Digger"}
-  delete playlist.kanye
-  return removeFromPlaylist(playlist, artistName )
+  delete playlist.artistname
+  playlist;
 }
